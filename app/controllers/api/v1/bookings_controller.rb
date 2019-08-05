45bfd841d71b0d60be019d9a4906d7cc10b3f737
Lin To <linxuanto@gmail.com>
2019-08-05 18:17:06 +0800

    bookings and products controllers

---
+++ app/controllers/api/v1/bookings_controller.rb
@@ -0,0 +1,42 @@
+class Api::V1::BookingsController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token
+  before_action :set_booking, only: [:show, :update]
+
+ # should be able to show an individual booking
+  def show
+  end
+
+  # create a new booking for an individual booking
+  def create
+    @booking = Booking.new(booking_params)
+      if @booking.save
+        render :show, status: :created
+      else
+        render_error
+      end
+  end
+
+  # Seller can update a booking status
+  def update
+    if @booking.update(booking_params)
+      render :show
+    else
+      render_error
+  end
+
+private
+
+  def set_bookings
+    @booking = Bookings.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:status, :completed, :date, :time, :user_id, :product_id)
+  end
+
+  def render_error
+    render json: { errors: @user.errors.full_messages },
+    status: :unprocessable_entity
+  end
+end
+
---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::BookingsController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def update
+    if @product.update(product_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def create
+    @product = Product.create(product_params)
+    if @product.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :location_lat, :location_long, :user_id)
+  end
+
+  def render_error
+    render json: { errors: @game.errors.full_messages },
+      status: :unprocessable_entity
+  end
+end
