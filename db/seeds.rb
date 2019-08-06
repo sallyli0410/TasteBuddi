02aae8485d03200c1f548397fcc4520e45a40a7e
Lin To <linxuanto@gmail.com>
2019-08-06 11:16:04 +0800

    continued edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 require 'date'
-require "open-uri"
-require "nokogiri"
+
 # <-----------------deleting all data------------------>
 
 puts "Cleaning all data"
@@ -13,11 +12,11 @@ Product.destroy_all
 puts "creating users"
 
 5.times do
-  u = User.create(
+  User.create(
     wx_name: Faker::Name.unique.name,
     wx_avatar: "",
     phone_number: Faker::Number.number(digits: 11),
-    seller_complete: :false,
+    seller_complete: :true,
     )
   end
 
@@ -31,28 +30,28 @@ product_attributes = [
     name:'Kale Pesto sauce',
     image: 'https://images.unsplash.com/photo-1519623286359-e9f3cbef015b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
     description: 'Home made fresh kale pesto',
-    location_lat: 0, #review wx choose location
-    location_long: 0, #review wx choose location
+    location_lat: 31.22184, #review wx choose location
+    location_long: 121.439884, #review wx choose location
     rating: 3,
-    user_id: u,
+    user_id: User.last,
   },
   {
     name: 'Malaysia Sambal Sauce',
     image: "http://theindigokitchen.com/wp-content/uploads/2016/06/IMG_7227__1464965797_86.82.108.171-1024x819-1160x665.jpg",
     description: 'Authentic Malaysian spicy sambal sauce',
-    location_lat: 0, #review wx choose location
-    location_long: 0, #review wx choose location
+    location_lat: 31.22184, #review wx choose location
+    location_long: 121.439884, #review wx choose location
     rating: 5,
-    user_id: u,
+    user_id: User.last,
   },
   {
     name: 'Kumquat Chilli Chuntney',
     image:"https://cdn-image.foodandwine.com/sites/default/files/styles/medium_2x/public/2014-r-xl-kumquat-and-pineapple-chutney.jpg?itok=-S4ebOu-",
     description: 'Delicous Sweet and Spicy kumquat, lime and chilli chuntney',
-    location_lat: 0, #review wx choose location
-    location_long: 0, #review wx choose location
+    location_lat: 31.22184, #review wx choose location
+    location_long: 121.439884, #review wx choose location
     rating: 5,
-    user_id: u,
+    user_id: User.last,
   }
 ]
 
