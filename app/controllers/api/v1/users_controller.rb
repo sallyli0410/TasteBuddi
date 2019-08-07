38b41b13798e695ec23d721a185cf067a4134b48
Lin To <linxuanto@gmail.com>
2019-08-07 14:40:20 +0800

    added a new controller model to update user login info

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::UsersController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
+  before_action :set_user, only: [:show, :update]
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     @bookings = @user.bookings
     @requests = Booking.joins(:product).where('products.user_id = ?', params[:id])
   end
@@ -16,7 +17,18 @@ class Api::V1::UsersController < Api::V1::BaseController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
 private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:wx_id, :wx_name, :wx_avatar, :phone_number, :seller_complete)
