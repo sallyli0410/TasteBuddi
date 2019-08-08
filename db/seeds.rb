1707747a44050758f488d106f5ab0a9a6289fd81
Sally Li <sallyli0410@gmail.com>
2019-08-08 17:24:46 +0800

    seeded rating

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ products = [
     name:'Kale Pesto sauce',
     image: 'https://images.unsplash.com/photo-1519623286359-e9f3cbef015b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
     description: 'Home made fresh kale pesto with fresh mint',
+    avg_rating: 3,
     location_lat: 30.0,
     location_long: 119,
     ingredients: ['basil', 'kale', 'olive oil']
@@ -19,6 +20,7 @@ products = [
     name: 'Malaysia Sambal Sauce',
     image: "http://theindigokitchen.com/wp-content/uploads/2016/06/IMG_7227__1464965797_86.82.108.171-1024x819-1160x665.jpg",
     description: 'Authentic Malaysian spicy sambal sauce',
+    avg_rating: 4,
     location_lat: 30.3,
     location_long: 119,
     ingredients: ['chilies', 'garlic', 'lemongrass', 'shallots', 'ginger']
@@ -27,6 +29,7 @@ products = [
     name: 'Kumquat Chilli Chuntney',
     image: "https://cdn-image.foodandwine.com/sites/default/files/styles/medium_2x/public/2014-r-xl-kumquat-and-pineapple-chutney.jpg?itok=-S4ebOu-",
     description: 'Delicous Sweet and Spicy kumquat, lime and chilli chuntney',
+    avg_rating: 5,
     location_lat: 30.9,
     location_long: 118,
     ingredients: ['orange juice', 'cranberry', 'cinnamon', 'cloves']
@@ -35,6 +38,7 @@ products = [
     name: 'Spicy Peanut Sauce',
     image: "https://cn.bing.com/th?id=OIP.7-Lxx7ZGkzTQMz62EYWI6AHaKL&pid=Api&rs=1",
     description: 'Spicy peanut sauce, perfect for satay marinated skewers and dipping',
+    avg_rating: 2,
     location_lat: 30.9,
     location_long: 118.8,
     ingredients: ['chilies', 'peanut']
@@ -43,6 +47,7 @@ products = [
     name: 'MARINARA SAUCE RECIPE',
     image: "https://cookieandkate.com/images/2018/03/best-marinara-sauce-recipe-2.jpg",
     description: 'Made with tomatoes, onion, garlic, and simple Italian herbs, enjoy this delicious and healthy Marinara Sauce Recipe with pasta, lasagna, pizza, or mixed into your favorite soups.',
+    avg_rating: 3,
     location_lat: 30.9,
     location_long: 118.8,
     ingredients: ['tomato', 'salt', 'black pepper', 'garlic', 'olive oil', 'white wine']
@@ -51,6 +56,7 @@ products = [
     name: 'Thai Sweet Chili Sauce',
     image: "http://www.chocolatemoosey.com/wp-content/uploads/2012/09/Sweet-Chili-Sauce-8973.jpg",
     description: 'Thai family recipe for sweet chilli sauce',
+    avg_rating: 5,
     location_lat: 34.9,
     location_long: 118.8,
     ingredients: ['white sugar', 'chilies', 'garlic', 'salt']
@@ -59,6 +65,7 @@ products = [
     name: 'Barbecue Sauce',
     image: "https://cn.bing.com/th?id=OIP.YcDSQHtk-qUq8dYBNNMc2gHaFj&pid=Api&rs=1",
     description: 'Sweet barbecue sauce with a little kick made Kansas City way',
+    avg_rating: 1,
     location_lat: 38.9,
     location_long: 119.8,
     ingredients: ['honey', 'ketchup', 'salt']
@@ -67,6 +74,7 @@ products = [
     name: 'Authentic Teriyaki Sauce',
     image: "https://cn.bing.com/th?id=OIP.AhDgMmNFONyVsJAqyjPxjwHaLL&pid=Api&rs=1",
     description: 'Umami Teriyaki sauce perfect for special occasions',
+    avg_rating: 3,
     location_lat: 30.9,
     location_long: 119.8,
     ingredients: ['soy sauce', 'sweet rice wine', 'garlic', 'ginger']
@@ -75,6 +83,7 @@ products = [
     name: 'Homemade Steak Sauce',
     image: "http://www.simplyscratch.com/wp-content/uploads/2013/03/steak-sauce-1.jpg",
     description: 'Peppery steak sauce',
+    avg_rating: 3,
     location_lat: 30.9,
     location_long: 119.8,
     ingredients: ['ketchup', 'onion', 'clove', 'garlic', 'mustard', 'soy sauce', 'olive oil', 'lemon juice', 'vinegar', 'brown sugar']
@@ -83,6 +92,7 @@ products = [
     name: 'Peri Peri Mayo',
     image: "https://cn.bing.com/th?id=OIP.h45brCr4WmMy0NLGOHXyMwHaFq&pid=Api&rs=1",
     description: 'Easy to make and taste just fantastic peri peri mayo',
+    avg_rating: 5,
     location_lat: 30.9,
     location_long: 119.8,
     ingredients: ['egg yolk', 'salt', 'mustard', 'lemon juice', 'vinegar']
