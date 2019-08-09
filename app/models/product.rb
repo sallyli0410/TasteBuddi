2245947e275af7e92b04c90348f287dd6ed0bed6
Nox Yu <nox19990209@gmail.com>
2019-08-09 16:32:46 +0800

    now deleting a product deletes all ingredients and reviews associated with that product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :ingredients
-  has_many :reviews
+  has_many :ingredients, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   acts_as_taggable_on :tags
   # acts_as_favoritable
 
